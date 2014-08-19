84e50056f1fe64d2ca7640e456e6a130938be7c4
tenbo07 <tenbo07@gmail.com>
2014-08-19 22:29:35 +0900

    fix tmp path

--- recipes/elasticsearch.rb
+++ recipes/elasticsearch.rb
@@ -1,12 +1,12 @@
 
 filename = node[:elasticsearch][:deb_url].split('/').last
-remote_file "tmp/#{filename}" do
+remote_file "/tmp/#{filename}" do
   source node[:elasticsearch][:deb_url]
   checksum node[:elasticsearch][:deb_sha]
   mode 00644
 end
 
-dpkg_package "tmp/#{filename}" do
+dpkg_package "/tmp/#{filename}" do
   action :install
 end
 
