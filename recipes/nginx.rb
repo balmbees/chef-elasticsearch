0a7781c7230c9bc3b8090ed931d9a34b93345584
tenbo07 <tenbo07@gmail.com>
2014-08-25 20:57:27 +0900

    nginx.conf template add

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -14,7 +14,14 @@ template '/etc/nginx/conf.d/kibana.conf' do
   mode '0644'
 end
 
+template '/etc/nginx/nginx.conf' do
+  source 'nginx.conf.erb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
 service 'nginx' do
   supports :status => true
-  action [ :enable, :start ]
+  action [ :enable, :restart ]
 end
